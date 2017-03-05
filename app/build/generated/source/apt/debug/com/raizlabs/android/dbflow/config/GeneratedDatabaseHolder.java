a1f210021fd18e8adbca229330604e9613ed4805
Zen <zuzen324@gmail.com>
2017-03-06 00:53:44 +0800

    add remaining word count when composing twitter

--- app/build/generated/source/apt/debug/com/raizlabs/android/dbflow/config/GeneratedDatabaseHolder.java
+++ app/build/generated/source/apt/debug/com/raizlabs/android/dbflow/config/GeneratedDatabaseHolder.java
@@ -17,12 +17,12 @@ import java.util.UUID;
 
 public final class GeneratedDatabaseHolder extends DatabaseHolder {
   public GeneratedDatabaseHolder() {
-    typeConverters.put(Date.class, new DateConverter());
     typeConverters.put(Boolean.class, new BooleanConverter());
     typeConverters.put(UUID.class, new UUIDConverter());
+    typeConverters.put(Date.class, new DateConverter());
+    typeConverters.put(BigDecimal.class, new BigDecimalConverter());
     typeConverters.put(Calendar.class, new CalendarConverter());
     typeConverters.put(GregorianCalendar.class, new CalendarConverter());
-    typeConverters.put(BigDecimal.class, new BigDecimalConverter());
     typeConverters.put(java.sql.Date.class, new SqlDateConverter());
     typeConverters.put(Time.class, new SqlDateConverter());
     typeConverters.put(Timestamp.class, new SqlDateConverter());
--- app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/mysimpletweets/ComposeActivity.java
@@ -3,6 +3,8 @@ package com.codepath.apps.mysimpletweets;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -15,22 +17,46 @@ import static com.raizlabs.android.dbflow.config.FlowManager.getContext;
 
 public class ComposeActivity extends AppCompatActivity {
 
-    private EditText etTwitterContent;
+    private EditText etTweetContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
 
-        etTwitterContent = (EditText) findViewById(R.id.etTweetContent);
+        EditText etTweetContent = (EditText) findViewById(R.id.etTweetContent);
         ImageView myPhoto = (ImageView) findViewById(ivMyPhoto);
         TextView myName = (TextView) findViewById(R.id.tvMyName);
         TextView myScreenName = (TextView) findViewById(R.id.tvMyScreenName);
+        final TextView tvRemainingWord = (TextView) findViewById(R.id.tvRemainingWord);
 
         myName.setText( getIntent().getStringExtra("myName"));
         myScreenName.setText( "@"+ getIntent().getStringExtra("myScreenName"));
         String url = getIntent().getStringExtra("myPhotoUrl");
         Picasso.with(getContext()).load(url).into(myPhoto);
+
+
+
+        etTweetContent.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+                int i = 140 - s.length();
+
+                tvRemainingWord.setText(Integer.toString(i));
+            }
+        });
 
     }
 
@@ -38,7 +64,6 @@ public class ComposeActivity extends AppCompatActivity {
 
 
     public void onPostTwitterAction(View v) {
-        EditText etTweetContent = (EditText) findViewById(R.id.etTweetContent);
         // Prepare data intent
         Intent data = new Intent();
         // Pass relevant data back as a result
